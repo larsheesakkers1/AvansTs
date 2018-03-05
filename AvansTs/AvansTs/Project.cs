25e87910656f404657bed07f5e56fcb5fad56942
Lars Heesakkers <larsheesakkers@live.nl>
2018-03-05 12:05:32 +0100

    user roles and editable sprint

--- AvansTs/AvansTs/Project.cs
+++ AvansTs/AvansTs/Project.cs
@@ -1,10 +1,24 @@
-﻿namespace AvansTs
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using AvansTs.vsts;
+
+namespace AvansTs
 {
     internal class Project
     {
         public void init(string projectName)
         {
+            List<User> userList = new List<User>();
+            List<Sprint> Project = new List<Sprint>();
+            userList.Add(new User("Lars","developer"));
+            userList.Add(new User("Cliff","Scrum Master"));
+            userList.Add(new User("Pascal","Productowner"));
 
+            var ScrumMaster = userList.Where(i => i.role == "Scrum Master").SingleOrDefault();
+            Project.Add(new Sprint(ScrumMaster, userList,DateTime.Parse("5/3/2018"), DateTime.Parse("9/3/2018")));
+            
         }
     }
 }
--- AvansTs/AvansTs/vsts/Sprint.cs
+++ AvansTs/AvansTs/vsts/Sprint.cs
@@ -1,6 +1,29 @@
-﻿namespace AvansTs.vsts
+﻿using System;
+using System.Collections.Generic;
+using AvansTs.vsts._BackLogItems;
+
+namespace AvansTs.vsts
 {
     internal class Sprint : IBackLog
     {
+        private User scrummaster;
+        private List<Item> items;
+        private DateTime startDate;
+        private DateTime endDate;
+
+        public Sprint(User scrummaster, List<User> restOfTeam, List<Item> items, DateTime startDate, DateTime endDate)
+        {
+            this.endDate = endDate;
+            this.startDate = startDate;
+            this.items = items;
+            this.scrummaster = scrummaster;
+        }
+
+        private bool isEditAble()
+        {
+            var today = DateTime.Today;
+            return today < startDate;
+        }
+
     }
 }
